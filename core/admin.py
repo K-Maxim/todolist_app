984a9d75873eeba20e540ad466e78cbf042cd428
K-Maxim <m.karpenko96@mail.ru>
2022-10-02 17:21:01 +0300

    user-admin

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+
+from .models import User
+
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    list_display = ('username', 'email', 'first_name', 'last_name')
+    search_fields = ('username', 'email', 'first_name', 'last_name')
+    readonly_fields = ('last_login', 'date_joined')
+    fieldsets = (
+        (None, {'fields': ('username',)}),
+        ('Personal Info', {'fields': ('first_name', 'last_name', 'email')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')})
+    )
