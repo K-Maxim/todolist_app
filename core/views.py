27c01fd760bc98f3ee9ee7fbab5f1db5825da0a9
K-Maxim <m.karpenko96@mail.ru>
2022-10-14 21:20:35 +0300

    API for create user

--- core/views.py
+++ core/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework.generics import CreateAPIView
+
+from core.models import User
+from core.serializer import UserRegistrationSerializer
+
+
+class UserCreate(CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserRegistrationSerializer
