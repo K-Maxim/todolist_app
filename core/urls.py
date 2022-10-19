34125c9847a536e513a626dc0b33a0830de56692
K-Maxim <m.karpenko96@mail.ru>
2022-10-19 21:20:20 +0300

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from core import views
 
 urlpatterns = [
-    path('signup', views.UserSignupView.as_view(), name='signup'),
+    path('sign-up', views.UserSignupView.as_view(), name='signup'),
     path('login', views.UserLoginView.as_view(), name='login'),
     path('profile', views.UserProfileView.as_view(), name='ret-up-dest'),
     path('update_password', views.UserUpdatePassword.as_view(), name='update-pass')
