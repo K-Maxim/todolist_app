9fdee6c1e0f334a2667f9319eaa72f379f4e10a3
K-Maxim <m.karpenko96@mail.ru>
2022-10-25 20:36:02 +0300

    created goals category

---
+++ goals/serializer.py
@@ -0,0 +1,22 @@
+from rest_framework import serializers
+
+from core.serializer import UserProfileSerializer
+from goals.models import GoalCategory
+
+
+class GoalCreateSerializer(serializers.ModelSerializer):
+    user = serializers.HiddenField(default=serializers.CurrentUserDefault())
+
+    class Meta:
+        model = GoalCategory
+        read_only_fields = ("id", "created", "updated", "user", 'is_delete')
+        fields = '__all__'
+
+
+class GoalCategorySerializer(serializers.ModelSerializer):
+    user = UserProfileSerializer(read_only=True)
+
+    class Meta:
+        model = GoalCategory
+        fields = "__all__"
+        read_only_fields = ("id", "created", "updated", "user")
--- todolist/settings.py
+++ todolist/settings.py
@@ -45,7 +45,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'social_django',
-    'core'
+    'core',
+    'goals',
 ]
 
 MIDDLEWARE = [
