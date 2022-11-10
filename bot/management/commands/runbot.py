f267be31be82968e1cb6c446cdd0f650db025788
K-Maxim <m.karpenko96@mail.ru>
2022-11-10 22:11:07 +0300

    verification user

--- bot/management/commands/runbot.py
+++ bot/management/commands/runbot.py
@@ -1,6 +1,10 @@
+import os
+
 from django.core.management import BaseCommand
 
+from bot.models import TgUser
 from bot.tg.client import TgClient
+from bot.tg.models import Message
 from todolist import settings
 
 
@@ -9,6 +13,31 @@ class Command(BaseCommand):
         super().__init__(*args, **kwargs)
         self.tg_client = TgClient(settings.BOT_TOKEN)
 
+    @staticmethod
+    def _generate_verification_code():
+        return os.urandom(12).hex()
+
+    def handle_unverified_user(self, massage: Message, tg_user: TgUser):
+        code: str = self._generate_verification_code()
+        tg_user.verification_code = code
+        tg_user.save(update_fields=('verification_code',))
+        self.tg_client.send_message(
+            chat_id=massage.chat.id,
+            text=f"[verification_code] {tg_user.verification_code}"
+        )
+
+    def handle_massage(self, message: Message):
+        tg_user, _ = TgUser.objects.select_related('user').get_or_create(
+            chat_id=message.chat.id,
+            defaults={
+                'username': message.from_.username
+            }
+        )
+        if tg_user.user:
+            self.tg_client.send_message(chat_id=message.chat.id, text='Вы уже зарегистрированы')
+        else:
+            self.handle_unverified_user(massage=message, tg_user=tg_user)
+
     def handle(self, *args, **options):
         offset = 0
         tg_client = TgClient(settings.BOT_TOKEN)
@@ -16,5 +45,4 @@ class Command(BaseCommand):
             res = tg_client.get_updates(offset=offset)
             for item in res.result:
                 offset = item.update_id + 1
-                self.tg_client.send_message(chat_id=item.message.chat.id, text=item.message.text)
-                print(item.message)
+                self.handle_massage(message=item.message)
