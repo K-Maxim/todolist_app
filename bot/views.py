938674c509bda23ebf8bee6cc93398ef3e0b78eb
K-Maxim <m.karpenko96@mail.ru>
2022-11-10 22:11:07 +0300

    verification user

--- bot/views.py
+++ bot/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from rest_framework.generics import GenericAPIView
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
-# Create your views here.
+from bot.models import TgUser
+from bot.serializers import TgUserSerializer
+from bot.tg.client import TgClient
+from todolist import settings
+
+
+class VerificationView(GenericAPIView):
+    model = TgUser
+    permission_classes = [IsAuthenticated]
+    serializer_class = TgUserSerializer
+
+    def patch(self, request, *args, **kwargs):
+        serializer: TgUserSerializer = self.get_serializer(request.data)
+        serializer.is_valid(raise_exception=True)
+
+        tg_user: TgUser = serializer.validated_data['tg_user']
+        tg_user.user = self.request.user
+        tg_user.save(update_fields=('user', ))
+
+        instance_serilalizer: TgUserSerializer = self.get_serializer(tg_user)
+        TgClient(settings.BOT_TOKEN).send_message(tg_user.chat_id, "[Верификация завершена]")
+        return Response(instance_serilalizer.data)
